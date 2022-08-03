0621eb7edb1e0061da4b0f319b1c898a142fe6c4
squidawg <alex.kronin@me.com>
2022-08-03 14:22:03 +0400

    feat:  finished pagination

--- scripts/paginationGenerator.js
+++ scripts/paginationGenerator.js
@@ -1,32 +1,32 @@
 import {generateCardAppend} from "./generateCardAppend.js";
+import {createElement} from "./createElement.js";
 import {PAGINATION} from "./pets.js";
-export const ITEM_SECOND = document.querySelector(".pagination_second");
-export const ITEM_THIRD = document.querySelector(".pagination_third");
-export const ITEM_FORTH = document.querySelector(".pagination_forth");
-export let FIRST_PAGE = document.querySelector(".pagination_first");
-export let LAST_PAGE = document.querySelector(".pagination_last");
+let timePets;
 
+function paginationPageCreator(element) {
+    for (let i = 0; i < element; i++) {
+        let page = createElement('div', 'page');
+        page.id = i + 1;
+        PAGINATION.append(page);
 
-
+    }
+}
+window.onresize = function (){
+    clearTimeout(timePets);
+    timePets = setTimeout(function (){
+            PAGINATION.innerHTML = ''
+            paginationGenerator()
+        }
+    , 100)
+}
 
 export function paginationGenerator(){
     let clientWidth = document.querySelector('.pagination_box').clientWidth;
-    let carouselItems = [ITEM_SECOND, ITEM_THIRD, ITEM_FORTH];
-
     let cardQuantity = clientWidth === 1200 ? 8 : clientWidth === 580 ? 6 : 3;
-    carouselItems.forEach((element) =>{
-        generateCardAppend(element, cardQuantity, 8);
+    paginationPageCreator( 48 / cardQuantity);
+    let page = [...PAGINATION.children];
+    page.forEach((element) =>{
+        generateCardAppend(element, cardQuantity, cardQuantity );
     });
-
-    FIRST_PAGE = ITEM_THIRD.cloneNode(true);
-    FIRST_PAGE.classList.replace('pagination_third', 'pagination_first')
-    FIRST_PAGE.id = 'first_page';
-
-    LAST_PAGE = ITEM_FORTH.cloneNode(true);
-    LAST_PAGE.classList.replace("pagination_forth", 'pagination_last');
-    LAST_PAGE.id = 'last_page';
-
-    PAGINATION.insertBefore(FIRST_PAGE, PAGINATION.firstChild);
-    PAGINATION.append(LAST_PAGE);
 
 }
--- scripts/pets.js
+++ scripts/pets.js
@@ -1,95 +1,102 @@
 import {readJSON} from "./readJSON.js";
-import {ITEM_SECOND, ITEM_FORTH, paginationGenerator, FIRST_PAGE, LAST_PAGE} from "./paginationGenerator.js";
-import {generateCardAppend} from "./generateCardAppend.js";
+import {paginationGenerator} from "./paginationGenerator.js";
 export const PAGINATION = document.querySelector('.pagination_box');
+const PAG_FIRST = document.querySelector('#pagination_first');
+const PAG_LAST = document.querySelector('#pagination_last');
 const PAG_RIGHT = document.querySelector('#pagination_right');
 const PAG_LEFT = document.querySelector('#pagination_left');
+
 const PAGE_COUNTER = document.querySelector('#page_counter');
-const PAG_LAST = document.querySelector('#pagination_last');
+let current = 0;
+
 
 async function init_pets(){
     await readJSON();
     paginationGenerator();
-    paginationBtnController();
+    PAG_LEFT.disabled = true;
+    PAG_FIRST.disabled = true;
 }
 
 
-const paginationBtnController = () => {
-    if(PAGE_COUNTER.innerHTML === '1'){
-        PAG_LEFT.disabled = true;
-    }
-    else if (PAGE_COUNTER.innerHTML === '8'){
+
+const moveRight = () => {
+    PAG_RIGHT.removeEventListener('click', moveRight)
+    PAG_LEFT.removeEventListener('click', moveLeft)
+    current++;
+    PAGE_COUNTER.innerHTML = current + 1
+    PAGINATION.style.transform = `translate(-${100 * (current)}%)`;
+    console.log()
+    if(current === PAGINATION.children.length - 1){
         PAG_RIGHT.disabled = true;
-    }
-    else if (PAGE_COUNTER.innerHTML === '7'){
-        PAG_RIGHT.removeEventListener('click', pagination_right);
-        PAG_RIGHT.addEventListener('click', pag_last);
+        PAG_LAST.disabled = true;
     }
     else {
-        PAG_LEFT.disabled = false;
         PAG_RIGHT.disabled = false;
+        PAG_LEFT.disabled = false;
+
+        PAG_FIRST.disabled = false;
+        PAG_LAST.disabled = false;
     }
 }
 
-const pagRight = () => {
-  if(parseInt(PAGE_COUNTER.innerHTML) < 7){
-      let number = parseInt(PAGE_COUNTER.innerHTML) + 1;
-      PAGE_COUNTER.innerHTML = number;
-  }
-}
-
-const pagination_right = () =>{
-    PAGINATION.classList.add('pagination-right');
-    PAG_RIGHT.removeEventListener('click', pagination_right);
-    PAG_LEFT.removeEventListener('click', pagination_left);
-}
-
-const pagination_left = () => {
-    PAGINATION.classList.add('pagination-left');
-    PAG_LEFT.removeEventListener('click', pagination_left);
-    PAG_RIGHT.removeEventListener('click', pagination_right);
-}
-
-const pag_last = () => {
-    PAGINATION.classList.add('pagination-last');
-    let number = parseInt(PAGE_COUNTER.innerHTML) + 1
-    PAGE_COUNTER.innerHTML = number;
-    PAG_RIGHT.removeEventListener('click', pagination_right);
-    PAG_LEFT.removeEventListener('click', pagination_left);
-}
-
-PAG_RIGHT.addEventListener('click', pagination_right);
-PAG_LEFT.addEventListener('click', pagination_left);
-PAG_LEFT.addEventListener('click', paginationBtnController);
-PAG_RIGHT.addEventListener('click', pagRight)
-PAGINATION.addEventListener('animationend', (animationEvent) => {
-    let switchedItem;
-    paginationBtnController();
-    if (animationEvent.animationName === "pagination-toLeft") {
-        PAGINATION.classList.remove("pagination-left");
-        switchedItem = ITEM_SECOND;
-
-    }
-    else if (animationEvent.animationName === "pagination-toLast" ){
-        PAGINATION.classList.remove('pagination-last');
-        switchedItem = LAST_PAGE;
+const moveLeft = () => {
+    PAG_LEFT.removeEventListener('click', moveLeft)
+    PAG_RIGHT.removeEventListener('click', moveRight)
+    current--;
+    PAGE_COUNTER.innerHTML = current + 1
+    PAGINATION.style.transform = `translate(-${100 * (current)}%)`;
+    if(current === 0){
+        PAG_LEFT.disabled = true
+        PAG_FIRST.disabled = true;
     }
     else {
-        PAGINATION.classList.remove("pagination-right");
-        switchedItem = ITEM_FORTH;
+        PAG_LEFT.disabled = false
+        PAG_RIGHT.disabled = false
+        PAG_LAST.disabled = false;
     }
+}
 
-    document.querySelector("#third_cards").innerHTML = "";
-    while (switchedItem.childNodes.length > 0) {
-        document.querySelector("#third_cards").append(switchedItem.childNodes[0]);
-    }
-    generateCardAppend(switchedItem, 8, 8);
+const moveFirst = () => {
+    PAG_LEFT.removeEventListener('click', moveLeft);
+    PAG_FIRST.removeEventListener('click', moveFirst);
+    current = 0;
+    PAGINATION.style.transform = `translate(${current}%)`;
+    PAGE_COUNTER.innerHTML = current + 1;
+    PAG_FIRST.disabled = true;
+    PAG_LEFT.disabled = true;
+    PAG_LAST.disabled = false;
+    PAG_RIGHT.disabled = false;
+}
+
+const moveLast = () => {
+    PAG_LAST.removeEventListener('click', moveLast);
+    PAG_RIGHT.removeEventListener('click', moveRight)
+    current = PAGINATION.children.length - 1;
+    PAGINATION.style.transform = `translate(-${100 * (current)}%)`;
+    PAGE_COUNTER.innerHTML = current + 1;
+
+    PAG_LAST.disabled = true;
+    PAG_RIGHT.disabled = true;
+
+    PAG_LEFT.disabled = false
+    PAG_FIRST.disabled = false;
+
+}
+
+PAG_RIGHT.addEventListener('click', moveRight);
+PAG_LEFT.addEventListener('click', moveLeft);
+
+PAG_FIRST.addEventListener('click', moveFirst);
+PAG_LAST.addEventListener('click', moveLast);
 
 
-    PAG_RIGHT.addEventListener("click", pagination_right);
-    PAG_LEFT.addEventListener("click", pagination_left);
+PAGINATION.addEventListener('transitionend',(event) =>{
+    PAG_RIGHT.addEventListener('click', moveRight);
+    PAG_LEFT.addEventListener('click', moveLeft);
+
+    PAG_FIRST.addEventListener('click', moveFirst);
+    PAG_LAST.addEventListener('click', moveLast);
 
 });
-
 
 init_pets();
